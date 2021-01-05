aeb222fd9815320a1361109e971860533cac6720
zhengzhaosheng <1350111595@qq.com>
2021-01-05 18:50:11 +0800

    用户服务。

---
+++ src/main/java/top/freedommxc/zws/zwsuserservice/ZwsuserserviceApplication.java
@@ -0,0 +1,13 @@
+package top.freedommxc.zws.zwsuserservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZwsuserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZwsuserserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/freedommxc/zws/zwsuserservice/ZwsuserserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.freedommxc.zws.zwsuserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZwsuserserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
